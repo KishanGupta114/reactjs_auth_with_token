92be49c597c46dad2ec0db3c79c530d9962c0094
kishan <kkishan114@gmail.com>
2020-03-15 16:24:22 +0530

    updated

--- reactjs_auth_with_token/src/App.js
+++ reactjs_auth_with_token/src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { Link, Switch, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Logout from './components/Logout'
+import Admin from './components/Admin'
 
-function App() {
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/logout" component={Logout} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+
+const A = () => {
+  return (
+      <div>
+        <h1>This is A Component</h1>
+        <Link to='/b'>B component</Link>
+      </div>
+    )
+}
+
+
+const B = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +51,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ reactjs_auth_with_token/src/components/Login.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+export default class Login extends Component {
+
+    constructor(props) {
+        const token = localStorage.getItem("token")
+
+        let loggedIn = true
+        if(token == null){
+            loggedIn = false
+        }
+
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            loggedIn
+        }
+        this.onChange = this.onChange.bind(this)
+        this.submitForm = this.submitForm.bind(this)
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    submitForm(e) {
+        e.preventDefault()
+        const { username, password } = this.state
+        //login stupid logic
+        if(username === "Hey" && password === "Hey") {
+            localStorage.setItem("token", "jkdsnvskjnvkjdnsvndskvndsklnvdsl")
+            this.setState({
+                loggedIn : true
+            })
+        }
+    }
+    render() {
+        if(this.state.loggedIn) {
+            return <Redirect to='/admin' />
+        }
+        return (
+            <div>
+               <h1>Login</h1> 
+
+               <form onSubmit={this.submitForm} >
+                    <input 
+                    type='text'
+                    placeholder='username' 
+                    name='username' 
+                    value={this.state.username} 
+                    onChange={this.onChange} />
+
+                    <br />
+
+                    <input 
+                    type='password' 
+                    placeholder='password' 
+                    name='password' 
+                    value={this.state.password} 
+                    onChange={this.onChange} />
+
+                    <br />
+
+                    <input type='submit' />
+               </form>
+               
+               <h6>*Hint=> username & password : Hey</h6>
+               
+            </div>
+        )
+    }
+}
